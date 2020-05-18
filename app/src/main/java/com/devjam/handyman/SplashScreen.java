5bb50888ef22fbffb248af66cf434f722282ef1f
harshh3010 <60236472+harshh3010@users.noreply.github.com>
2020-05-18 19:00:06 +0530

    Added comments in SplashScreen.java

--- app/src/main/java/com/devjam/handyman/SplashScreen.java
+++ app/src/main/java/com/devjam/handyman/SplashScreen.java
@@ -27,16 +27,27 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        // Delaying the process by 1 second
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+
+                // Checking is the user is already logged-in from current device
+
                 if(firebaseAuth.getCurrentUser() != null){
+
+                    // Loading user's data from Firebase firestore
+
                     db.collection("Users")
                             .document(firebaseAuth.getCurrentUser().getEmail())
                             .get()
                             .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                                 @Override
                                 public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                                    // On successfully loading the data, storing it in the userApi object
+
                                     User user = documentSnapshot.toObject(User.class);
                                     userApi.setId(user.getId());
                                     userApi.setName(user.getName());
@@ -45,16 +56,26 @@ public class SplashScreen extends AppCompatActivity {
                                     userApi.setContact(user.getContact());
                                     userApi.setPincode(user.getPincode());
 
+                                    // Redirecting the user to Home Screen
+
                                     startActivity(new Intent(SplashScreen.this,HomeActivity.class));
                                     finish();
                                 }
                             }).addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
+
+                            // Displaying a toast message and closing the app on failure in loading data
+
                             Toast.makeText(SplashScreen.this,"Unable to load user data!",Toast.LENGTH_LONG).show();
+                            finish();
                         }
                     });
+
                 }else{
+
+                    // If user is not logged-in redirecting him to login screen
+
                     startActivity(new Intent(SplashScreen.this,LoginActivity.class));
                     finish();
                 }
