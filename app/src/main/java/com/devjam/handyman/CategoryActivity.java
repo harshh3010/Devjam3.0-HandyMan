47265facf3e40e6692076f97f44139a25e36c23b
harshh3010 <60236472+harshh3010@users.noreply.github.com>
2020-05-18 19:37:47 +0530

    Added comments in CategoryActivity.java

--- app/src/main/java/com/devjam/handyman/CategoryActivity.java
+++ app/src/main/java/com/devjam/handyman/CategoryActivity.java
@@ -33,18 +33,23 @@ public class CategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
+        // Initializing the required fields
         recyclerView = findViewById(R.id.category_service_recycler_view);
         progressBar = findViewById(R.id.category_activity_progress_bar);
 
+        // Displaying the progress bar until data is loaded
         progressBar.setVisibility(View.VISIBLE);
         recyclerView.setVisibility(View.GONE);
 
+        // Loading the data fetched from Home Activity
         city = getIntent().getStringExtra("city");
         category = getIntent().getStringExtra("category");
         ((TextView)findViewById(R.id.category_title_text)).setText(category);
 
+        // Loading the services under the current category using this method
         loadServices();
 
+        // Closing the activity on pressing back arrow
         findViewById(R.id.category_back_image).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,6 +60,8 @@ public class CategoryActivity extends AppCompatActivity {
     }
 
     private void loadServices(){
+
+        // Loading the services under given category from firebase firestore
         db.collection("Cities")
                 .document(city)
                 .collection("Services")
@@ -64,14 +71,19 @@ public class CategoryActivity extends AppCompatActivity {
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                        // On successfully loading the services adding them into an array list
                         services = new ArrayList<>();
                         for(QueryDocumentSnapshot ds : queryDocumentSnapshots){
                             services.add(ds.toObject(Service.class));
                         }
+
+                        // Displaying the services stored in array list in a recyclerview
                         adapter = new ServiceAdapter(services,city);
                         recyclerView.setAdapter(adapter);
                         recyclerView.setLayoutManager(new LinearLayoutManager(CategoryActivity.this));
 
+                        // Hiding the progress bar after all the data is displayed in recyclerview
                         recyclerView.setVisibility(View.VISIBLE);
                         progressBar.setVisibility(View.GONE);
                     }
