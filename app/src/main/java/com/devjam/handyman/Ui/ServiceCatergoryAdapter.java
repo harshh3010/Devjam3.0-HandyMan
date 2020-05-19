87ed01fcfb9151596a8364849afab1094d29bc2f
harshh3010 <60236472+harshh3010@users.noreply.github.com>
2020-05-19 18:48:36 +0530

    Added comments in ServiceCategoryAdapter.java

--- app/src/main/java/com/devjam/handyman/Ui/ServiceCatergoryAdapter.java
+++ app/src/main/java/com/devjam/handyman/Ui/ServiceCatergoryAdapter.java
@@ -33,6 +33,7 @@ public class ServiceCatergoryAdapter extends RecyclerView.Adapter<ServiceCatergo
     @NonNull
     @Override
     public ViewHolderClass onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflating the item view to be displayed in recyclerview
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view_service_category,parent,false);
         context = parent.getContext();
         return new ViewHolderClass(view);
@@ -40,14 +41,17 @@ public class ServiceCatergoryAdapter extends RecyclerView.Adapter<ServiceCatergo
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolderClass holder, int position) {
+        // Displaying the necessary data in their respective fields
         holder.name_txt.setText(myArr.get(position));
     }
 
+    // Method to get the size of categories array list
     @Override
     public int getItemCount() {
         return myArr.size() ;
     }
 
+    // Method to filter the contents of the recyclerview on basis of a query text
     @Override
     public Filter getFilter() {
         return filter;
@@ -87,11 +91,16 @@ public class ServiceCatergoryAdapter extends RecyclerView.Adapter<ServiceCatergo
 
         public ViewHolderClass(@NonNull View itemView) {
             super(itemView);
+
+            // Initializing the required fields
             name_txt = itemView.findViewById(R.id.item_service_category_name_text);
 
+            // On click listener for item view
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+
+                    // passing the City name and Category to CategoryActivity
                     if(!name_txt.getText().toString().isEmpty()){
                         Intent intent = new Intent(context, CategoryActivity.class);
                         intent.putExtra("category",name_txt.getText().toString());
